59c9a0e65a2b619694e087b6acb2795147e163f6
Shaikh Mohammed Talib <shaikhtalib2705@gmail.com>
2025-11-25 16:52:08 +0530

    updated this depricated app for redirection to the new one

--- src/components/Alert.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-const Alert = ({ type, message }) => {
-    return <div style={{ borderTopRightRadius: '0', borderTopLeftRadius: '0', borderBottomRightRadius: '10px', borderBottomLeftRadius: '10px' }} className={`${type} `} role="alert"> {message} </div>
-}
-
-export default Alert;
